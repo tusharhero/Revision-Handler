b67c9ab54b4c00ecbed3f8be4b3810ef0aa32da0
AnandhKumar E S <anandhkumares@gmail.com>
2022-10-20 19:14:56 +0530

    Added chapter progress
    Now in detailed view chapter progress is shown, topic to completed if  it is revised in last 7 days

--- main.py
+++ main.py
@@ -6,29 +6,47 @@ JSON_PATH = "example.json"
 
 # Print Current Date
 current_time = datetime.now()
-print(current_time.strftime("%m/%d/%Y, %H:%M"))
+print(f"Time - {current_time.strftime('%m/%d/%Y, %H:%M')}")
 
 short_data_view = {}
+
 
 def get_short_view():
     for subject in data.copy():
         for i, chapter in enumerate(data[subject]):
-            short_data_view[f"{subject[:2]}{i+1}"] = data[subject][chapter]  
+            short_data_view[f"{subject[:2]}{i+1}"] = data[subject][chapter]
+
 
 def detailed_view():
     for subject in data.copy():
         print(subject.upper())
         for i, chapter in enumerate(data[subject]):
-            print(f" - {chapter.capitalize()}")
+            total_topics = 0
+            topics_revised = 0
+            subtopic_data = ""
+
+            # Gets topics revised in last week and prepare topics to print
             for subtopic in data[subject][chapter]:
-                print(f"\t - {subtopic}  ({data[subject][chapter][subtopic]})")
+                revised_date = data[subject][chapter][subtopic]
+                total_topics += 1
+                days_past = (
+                    current_time - datetime.strptime(revised_date, "%m/%d/%Y")
+                ).days
+
+                if days_past <= 7:
+                    topics_revised += 1
+                subtopic_data += f"\t - {subtopic}  ({revised_date})\n"
+
+            # Prints all info about chapter
+            chapter_progress = int(topics_revised / total_topics * 100)
+            print(f" - {chapter.capitalize()} ({chapter_progress}%)\n" + subtopic_data)
         print()
 
 
 with open(JSON_PATH) as f:
     data = load(f)
 
-
+detailed_view()
 get_short_view()
 running = True
 while running:
